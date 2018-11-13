358d0df7060b82190408543b81edb26fcd0f5f67
Troels Ugilt Jensen <tujensen@gmail.com>
2018-11-13 12:52:55 +0100

    FOR-70: Added new address formatter for DAWA addresses

---
+++ web/modules/custom/foreningsmentor/src/Plugin/Field/FieldFormatter/CleanAddressDawaFormatter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\foreningsmentor\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\address_dawa\AddressDawaItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation for the 'address_dawa' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "address_dawa_clean",
+ *   label = @Translation("Clean Address DAWA"),
+ *   field_types = {
+ *     "address_dawa",
+ *   },
+ * )
+ */
+class CleanAddressDawaFormatter extends FormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    /** @var \Drupal\address_dawa\AddressDawaItemInterface $item */
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#prefix' => '<span translate="no">',
+        '#suffix' => '</span>',
+      ];
+      $elements[$delta] += $this->viewElement($item);
+
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Builds a renderable array for a single dawa address item.
+   *
+   * @param \Drupal\address_dawa\AddressDawaItemInterface $item
+   *   The address.
+   *
+   * @return array
+   *   A renderable array.
+   */
+  protected function viewElement(AddressDawaItemInterface $item) {
+    $value = [
+      $item->getData()["adressebetegnelse"],
+    ];
+    $element = [
+      '#type' => 'markup',
+      '#markup' => implode('', $value),
+    ];
+    return $element;
+  }
+}
