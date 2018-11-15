7c3d2a9f85f5de6e2cbecfca77aa08bdcaf919ea
Jesper Pedersen <jesper@yepz.dk>
2018-11-15 09:56:44 +0100

    FOR-93: Config export. Template updates. Add optimization to webpack.

--- web/themes/custom/foreningsmentor_theme/webpack.config.js
+++ web/themes/custom/foreningsmentor_theme/webpack.config.js
@@ -2,12 +2,24 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   entry: { main: './src/app.js' },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'app.js'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
   module: {
     rules: [
