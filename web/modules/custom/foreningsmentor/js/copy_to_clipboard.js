5b94f41532a1c4fc4d74259c40ca2faed5590432
Troels Ugilt Jensen <tujensen@gmail.com>
2018-11-16 14:15:40 +0100

    FOR-34: Started clipboard function

---
+++ web/modules/custom/foreningsmentor/js/copy_to_clipboard.js
@@ -0,0 +1,49 @@
+(function ($, Drupal) {
+  Drupal.AjaxCommands.prototype.copyToClipboardCommand = function (ajax, response) {
+    console.log(ajax, response);
+    function fallbackCopyTextToClipboard(text) {
+      var textArea = document.createElement("textarea");
+      textArea.value = text;
+      textArea.id = 'copy-to-clipboard-area';
+      document.body.appendChild(textArea);
+
+      textArea.focus();
+      textArea.select();
+
+      try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+
+        // If not successful fallback to window.prompt.
+        if (!successful) {
+          window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+        }
+      } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+      }
+
+      document.body.removeChild(textArea);
+    }
+
+    function copyTextToClipboard(text) {
+      navigator.clipboard.writeText(text).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+      }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+      });
+    }
+
+    navigator.permissions.query({
+      name: 'clipboard-write'
+    }).then(function (permissionStatus) {
+      console.log(permissionStatus);
+      if (permissionStatus === 'granted' && navigator.clipboard) {
+        copyTextToClipboard()
+      }
+      else {
+        fallbackCopyTextToClipboard(response.list);
+      }
+    });
+  };
+})(jQuery, Drupal);
---
+++ web/modules/custom/foreningsmentor/src/Ajax/CopyToClipboardCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\foreningsmentor\Ajax;
+
+use Drupal\Core\Ajax\CommandInterface;
+
+class CopyToClipboardCommand implements CommandInterface {
+  protected $list;
+
+  /**
+   * CopyToClipboardCommand constructor.
+   */
+  public function __construct($list) {
+    $this->list = $list;
+  }
+
+  public function render() {
+    $result = '';
+
+    $last_key = array_search(end($this->list), $this->list);
+    foreach ($this->list as $key => $object) {
+      $result = $result . '"' . $object . '"' . '<' . $object . '>';
+      if ($key != $last_key) {
+        $result .= '; ';
+      }
+    }
+
+    return [
+      'command' => 'copyToClipboardCommand',
+      'list' => $result,
+    ];
+  }
+
+}
