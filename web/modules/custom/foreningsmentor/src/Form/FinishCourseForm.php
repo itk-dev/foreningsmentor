6b3845edde9d14f38d0f52b77a5bccd909023771
Troels Ugilt Jensen <tujensen@gmail.com>
2019-01-29 10:57:11 +0100

    FOR-143: Added journal note and removed mentor when choosing A3 as finish status

--- web/modules/custom/foreningsmentor/src/Form/FinishCourseForm.php
+++ web/modules/custom/foreningsmentor/src/Form/FinishCourseForm.php
@@ -6,6 +6,8 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\Node;
 use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\user\Entity\User;
 
 /**
  * Class JournalForm.
@@ -22,17 +24,23 @@ class FinishCourseForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state, $node = NULL) {
-    $form['finish_course'] = array(
+  public function buildForm(
+    array $form,
+    FormStateInterface $form_state,
+    $node = NULL
+  ) {
+    $form['finish_course'] = [
       '#type' => 'details',
       '#title' => t('Finish course'),
       '#weight' => 5,
       '#open' => FALSE,
-    );
+    ];
 
     // Extract options for mentor end status.
     $vid = 'mentors_end_status';
-    $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid);
+    $terms = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadTree($vid);
     $options = [];
     foreach ($terms as $term) {
       $options[$term->tid] = $term->name;
@@ -96,6 +104,30 @@ class FinishCourseForm extends FormBase {
     $parentNode->set('field_mentors_end_status', $mentor_end_status);
     $parentNode->set('field_mentors_end_status_text', $mentor_end_status_text);
     $parentNode->set('field_finished', TRUE);
+
+    // If mentor end status == A3 remove the mentor from the course, to make the
+    // child return to the waiting list.
+    $mentor_status_term = Term::load($mentor_end_status);
+    $term_name = $mentor_status_term->getName();
+    if ($term_name == 'A3') {
+      $mentor_id = $parentNode->get('field_mentor')->target_id;
+      if (isset($mentor_id)) {
+        $mentor = User::load($mentor_id);
+
+        $journal_entry = Node::create([
+          'type' => 'journal_entry',
+          'title' => $this->t('System message: Mentor removed.'),
+          'body' => $this->t('Mentor @name removed from the course due to A3 status.',
+            ['@name' => $mentor->getDisplayName()]),
+          'field_only_for_coordinators' => TRUE,
+        ]);
+
+        $journal_entry->save();
+
+        $parentNode->get('field_diary')->appendItem($journal_entry);
+        $parentNode->set('field_mentor', []);
+      }
+    }
 
     $parentNode->save();
   }
