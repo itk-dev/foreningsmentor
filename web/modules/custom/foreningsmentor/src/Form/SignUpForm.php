392de5ca1278927f3cc219b8bee27c5be5b5b2c8
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-12-02 13:17:40 +0100

    FOR-224: Added signup form

---
+++ web/modules/custom/foreningsmentor/src/Form/SignUpForm.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\foreningsmentor\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\taxonomy\Entity\Term;
+
+/**
+ * Implements signup form.
+ */
+class SignUpForm extends FormBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId()
+  {
+    return 'sign_up_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $form['parent_name'] = [
+      '#type' => 'textfield',
+      '#required' => true,
+      '#title' => $this->t('Your name'),
+    ];
+    $form['child_name'] = [
+      '#type' => 'textfield',
+      '#required' => true,
+      '#title' => $this->t('Your child\'s name'),
+    ];
+    $form['phone_number'] = [
+      '#type' => 'tel',
+      '#required' => true,
+      '#title' => $this->t('Your phone number'),
+    ];
+    $form['mail'] = [
+      '#type' => 'textfield',
+      '#required' => true,
+      '#title' => $this->t('Your e-mail address'),
+    ];
+
+    $tids = \Drupal::entityQuery('taxonomy_term')->condition('vid', 'neighborhood')->execute();
+    $terms = Term::loadMultiple($tids);
+
+    $areaOptions = [];
+
+    foreach ($terms as $term) {
+      $areaOptions[$term->id()] = $term->getName();
+    }
+
+    $form['area'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Area'),
+      '#required' => true,
+      '#options' => $areaOptions,
+    ];
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Sign up'),
+      '#button_type' => 'primary',
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+    if (strlen($form_state->getValue('phone_number')) < 8) {
+      $form_state->setErrorByName('phone_number', $this->t('The phone number is too short. Please enter a full phone number.'));
+    }
+    $email = $form_state->getValue('email');
+    if ($email !== '' && !\Drupal::service('email.validator')->isValid($email)) {
+      $form_state->setErrorByName('email', $this->t('The email address %mail is not valid.', ['%mail' => $email,]));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    // @TODO: Send notification mail to coordinator for the given area.
+  }
+
+}
