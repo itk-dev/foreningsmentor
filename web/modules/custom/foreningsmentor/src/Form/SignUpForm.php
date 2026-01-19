0fea75b72d6dc87676619b1769f0501ebf38ae09
martinyde <yde001@gmail.com>
2026-01-19 11:30:39 +0100

    Added signup entity and code to match

--- web/modules/custom/foreningsmentor/src/Form/SignUpForm.php
+++ web/modules/custom/foreningsmentor/src/Form/SignUpForm.php
@@ -5,6 +5,7 @@ namespace Drupal\foreningsmentor\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
@@ -105,31 +106,99 @@ final class SignUpForm extends FormBase {
       '#type' => 'textfield',
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
-      '#title' => $this->t("Your child's name"),
+      '#title' => $this->t("Child's name"),
     ];
     $form['wrapper']['child']['birth_date'] = [
-      '#type' => 'textfield',
+      '#type' => 'date',
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
-      '#title' => $this->t("Your child's birth date"),
+      '#title' => $this->t("Child's birth date"),
     ];
     $form['wrapper']['child']['sex'] = [
-      '#type' => 'textfield',
+      '#type' => 'select',
+      '#options' => ['Dreng' => t('Boy'), 'Pige' => t('Girl')],
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
-      '#title' => $this->t("Your child's sex"),
+      '#title' => $this->t("Child's sex"),
     ];
     $form['wrapper']['child']['school'] = [
       '#type' => 'textfield',
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
-      '#title' => $this->t("Your child's school"),
+      '#title' => $this->t("Child's school"),
     ];
     $form['wrapper']['child']['grade'] = [
-      '#type' => 'textfield',
+      '#type' => 'select',
+      '#options' => [
+        '0. klasse' => '0. klasse',
+        '1. klasse' => '1. klasse',
+        '2. klasse' => '2. klasse',
+        '3. klasse' => '3. klasse',
+        '4. klasse' => '4. klasse',
+        '5. klasse' => '5. klasse',
+        '6. klasse' => '6. klasse',
+        '7. klasse' => '7. klasse',
+        '8. klasse' => '8. klasse',
+        '9. klasse' => '9. klasse',
+        '10. klasse' => '10. klasse',
+        'Andet' => 'Andet',
+      ],
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
-      '#title' => $this->t("Your child's grade"),
+      '#title' => $this->t("Child's grade"),
+    ];
+
+    $form['wrapper']['referer_check'] = [
+      '#prefix' => '<div class="border-bottom mb-3 pb-3">',
+      '#suffix' => '</div>',
+      '#type' => 'checkbox',
+      '#title' => $this->t('I am a referring a child on behalf of someone else.'),
+      '#description' => $this->t('<small>Check this box if you are not the childs parent. This will allow you to provide the referer\'s contact information.</small>'),
+    ];
+
+    $form['wrapper']['referer'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => 'border-bottom mb-3 pb-3',
+      ],
+      '#states' => [
+        'visible' => [
+          ':input[name="referer_check"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['wrapper']['referer']['referer_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Referer name'),
+      '#attributes' => ['class' => ['form-control', 'mb-3']],
+      '#states' => [
+        'required' => [
+          ':input[name="referer_check"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['wrapper']['referer']['referer_mail'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Referer e-mail'),
+      '#attributes' => ['class' => ['form-control', 'mb-3']],
+      '#states' => [
+        'required' => [
+          ':input[name="referer_check"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['wrapper']['referer']['referer_phone'] = [
+      '#type' => 'tel',
+      '#title' => $this->t('Referer phone number'),
+      '#attributes' => ['class' => ['form-control', 'mb-3']],
+      '#states' => [
+        'required' => [
+          ':input[name="referer_check"]' => ['checked' => TRUE],
+        ],
+      ],
     ];
 
     $form['wrapper']['parent'] = [
@@ -148,20 +217,21 @@ final class SignUpForm extends FormBase {
       '#type' => 'textfield',
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
-      '#title' => $this->t('Your name'),
+      '#title' => $this->t('Parent name'),
     ];
     $form['wrapper']['parent']['phone_number'] = [
       '#type' => 'tel',
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
-      '#title' => $this->t('Your phone number'),
+      '#title' => $this->t('Parent phone number'),
     ];
     $form['wrapper']['parent']['mail'] = [
       '#type' => 'email',
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
-      '#title' => $this->t('Your e-mail address'),
+      '#title' => $this->t('Parent e-mail address'),
     ];
+
     $form['wrapper']['other'] = [
       '#type' => 'container',
       '#attributes' => ['class' => ['row', 'no-gutters']],
@@ -175,9 +245,9 @@ final class SignUpForm extends FormBase {
     }
 
     $form['wrapper']['other']['address'] = [
-      '#prefix' => '<div class="col-md-12">',
-      '#suffix' => '</div>',
-      '#type' => 'textarea',
+      '#prefix' => '<div class="col-md-12"><p class="dawa-address" translate="no">',
+      '#suffix' => '</p></div>',
+      '#type' => 'textfield',
       '#required' => TRUE,
       '#attributes' => ['class' => ['form-control', 'mb-3']],
       '#title' => $this->t('Address'),
@@ -258,12 +328,57 @@ final class SignUpForm extends FormBase {
     $areaTerm = $this->entityTypeManager->getStorage('taxonomy_term')->load($area);
     // Overwrite $params area id with actual term name for display in mail.
     $params['form_values']['area'] = $areaTerm->getName();
+    $params['area_id'] = $areaTerm->id();
     // Send mail to coordinators assigned to the neighborhood.
     $params['site_name'] = $this->config('system.site')->get('name');
+    $params['site_url'] = Url::fromUserInput('/',  array('absolute' => 'true'))->toString();
+
+    $node = [
+      'title' => $params['form_values']['child_name'],
+      'type' => 'signup',
+      'field_name' => $params['form_values']['child_name'],
+      'field_birthday' => $params['form_values']['birth_date'],
+      'field_sex' => $params['form_values']['sex'],
+      'field_school' => $params['form_values']['school'],
+      'field_shool_class' => $params['form_values']['grade'],
+      'field_parent_name' => $params['form_values']['parent_name'],
+      'field_phone' => $params['form_values']['phone_number'],
+      'field_email' => $params['form_values']['mail'],
+      'field_address' => [
+        'id' => '0',
+        'value' => $params['form_values']['address'],
+        'type' => 'adresse',
+        'data' => [],
+        'status' => 0,
+        'lat' => 0,
+        'lng' => 0,
+      ],
+      'field_referer' => $params['form_values']['referer_name'],
+      'field_referer_phone' => $params['form_values']['referer_phone'],
+      'field_referer_email' => $params['form_values']['referer_mail'],
+      'field_activity_wishes' => $params['form_values']['activity'],
+      'field_neighborhood' => $area,
+      'field_archived' => FALSE,
+    ];
+
+    $node =$this->entityTypeManager->getStorage('node')->create($node);
+    $node->save();
+
+    $params['node_id'] = $node->id();
+
+    // Notify coordinators assigned to the neighborhood.
     foreach ($neighborhoodUsers as $user) {
       if ($user->hasRole('coordinator')) {
         $this->mailManager->mail('foreningsmentor', 'foreningsmentor_signup', $user->get('mail')->value, $this->languageManager->getDefaultLanguage()->getName(), $params);
       }
+    }
+
+    // Notify parent.
+    $this->mailManager->mail('foreningsmentor', 'foreningsmentor_parent_signup', $params['form_values']['mail'], $this->languageManager->getDefaultLanguage()->getName(), $params);
+
+    // Notify referer.
+    if ($params['form_values']['referer_mail']) {
+      $this->mailManager->mail('foreningsmentor', 'foreningsmentor_referer_signup', $params['form_values']['referer_mail'], $this->languageManager->getDefaultLanguage()->getName(), $params);
     }
 
     $this->messenger()->addStatus($this->t('Thank you for signing up. You will be contacted by @site_name shortly.', ['@site_name' => $params['site_name']]));
